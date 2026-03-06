5b24c76013b42efacde0b7d12ec28ba718cd798a
ShubhamBadadale <shubhambadadale@gmail.com>
2026-03-06 16:20:39 +0530

    added basic trading dashboard UI

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>TradeEngine Simulator</h1>
+      <p>Virtual stock trading platform for learning investment strategies.</p>
+    </div>
+  );
+}
+
+export default App;
